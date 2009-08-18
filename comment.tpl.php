9951ca9bbdb6156c54323dd60abc27688956859b
couzinhub <couzinhub@133581.no-reply.drupal.org>
2009-08-18 23:47:31 +0000

    update from d7 version ported to 6

--- comment.tpl.php
+++ comment.tpl.php
@@ -1,65 +1,32 @@
-<?php
-// $Id$
-
-/**
- * @file comment.tpl.php
- * Default theme implementation for comments.
- *
- * Available variables:
- * - $author: Comment author. Can be link or plain text.
- * - $content: Body of the post.
- * - $date: Date and time of posting.
- * - $links: Various operational links.
- * - $new: New comment marker.
- * - $picture: Authors picture.
- * - $signature: Authors signature.
- * - $status: Comment status. Possible values are:
- *   comment-unpublished, comment-published or comment-preview.
- * - $submitted: By line with date and time.
- * - $title: Linked title.
- *
- * These two variables are provided for context.
- * - $comment: Full comment object.
- * - $node: Node object the comments are attached to.
- *
- * @see template_preprocess_comment()
- * @see theme_comment()
- */
-?>
-
-<div class="comment <?php echo $classes .' '. $zebra; if ($unpublished) { echo 'comment-unpublished'; } ?> clear-block">
+<div class="comment <?php print $classes .' '. $zebra; if ($unpublished) { print 'unpublished'; } ?> clear-block">
 	<div class="comment-inner">
 		
     <?php if ($title): ?>
-      <h3 class="title"><?php echo $title; if (!empty($new)): ?> <span class="new"><?php echo $new; ?></span><?php endif; ?></h3>
-    <?php elseif (!empty($new)): ?>
-      <div class="new"><?php echo $new; ?></div>
+      <h3 class="title"><?php print $title ?></h3>
     <?php endif; ?>
-    
-    <?php if ($unpublished): ?>
-      <div class="unpublished"><?php echo t('Unpublished'); ?></div>
+
+    <?php if ($new) : ?>
+      <span class="new"><?php print drupal_ucfirst($new) ?></span>
     <?php endif; ?>
-    
-    <?php if ($picture): ?>
-	    <div class="picture"><?php echo $picture; ?></div>
-	  <?php endif; ?>
+        
+    <?php print $user_picture; ?>
 	    
     <div class="submitted">
-      <?php echo $submitted; ?>
+      <?php print $submitted; ?>
     </div>
     
     <div class="content">
-      <?php echo $content ?>
+      <?php print $content ?>
       <?php if ($signature): ?>
       <div class="user-signature clear-block">
-        <?php echo $signature; ?>
+        <?php print $signature; ?>
       </div>
       <?php endif; ?>
     </div>
     
     <?php if ($links): ?>
       <div class="links">
-        <?php echo $links; ?>
+        <?php print $links; ?>
       </div>
     <?php endif; ?>  
 
