900baf32e34dcc2c835740cd1f12694750cea038
couzinhub <couzinhub@133581.no-reply.drupal.org>
2009-10-17 00:10:14 +0000

    updates for code slush

--- theme-settings.php
+++ theme-settings.php
@@ -1,26 +1,6 @@
 <?php
-
-// Implementation of THEMEHOOK_settings() function.
-
-// @param $saved_settings
-//   array An array of saved settings for this theme.
-// @return
-//   array A form array.
-
-function phptemplate_settings($saved_settings) {
-  
-  // The default values for the theme variables. Make sure $defaults exactly
-  // matches the $defaults in the template.php file.
-  
-  $defaults = array(
-    'zen_tabs'        => 1,
-    'wireframe_mode'  => 0,
-    'block_editing'   => 0,
-    'clear_registry'  => 0,
-  );
-
-  // Merge the saved variables and their default values
-  $settings = array_merge($defaults, $saved_settings);
+// Form override fo theme settings
+function basic_form_system_theme_settings_alter(&$form, $form_state) {
 
   $form['options_settings'] = array(
     '#type' => 'fieldset',
@@ -32,7 +12,6 @@ function phptemplate_settings($saved_settings) {
     '#type' => 'checkbox',
     '#title' =>  t('Use the ZEN tabs'),
     '#description'   => t('Check this if you wish to replace the default tabs by the ZEN tabs'),
-    '#default_value' => $settings['zen_tabs']
   );
 
   $form['dev_settings'] = array(
@@ -45,21 +24,16 @@ function phptemplate_settings($saved_settings) {
     '#type' => 'checkbox',
     '#title' =>  t('Wireframe Mode - Display borders around main layout elements'),
     '#description'   => t('<a href="!link">Wireframes</a> are useful when prototyping a website.', array('!link' => 'http://www.boxesandarrows.com/view/html_wireframes_and_prototypes_all_gain_and_no_pain')),
-    '#default_value' => $settings['wireframe_mode']
   );
   $form['dev_settings']['block_editing'] = array(
     '#type' => 'checkbox',
     '#title' =>  t('Show block editing on hover'),
     '#description'   => t('When hovering over a block, privileged users will see block editing links. <strong>You might need to rebuild the theme registry after activating this option.</strong>'),
-    '#default_value' => $settings['block_editing']
   );
   $form['dev_settings']['clear_registry'] = array(
     '#type' => 'checkbox',
     '#title' =>  t('Rebuild theme registry on every page.'),
     '#description'   =>t('During theme development, it can be very useful to continuously <a href="!link">rebuild the theme registry</a>. WARNING: this is a huge performance penalty and must be turned off on production websites.', array('!link' => 'http://drupal.org/node/173880#theme-registry')),
-    '#default_value' => $settings['clear_registry']
   );
   
-  // Return the additional form widgets
-  return $form;
 }
