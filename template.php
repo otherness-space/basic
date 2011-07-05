fa4d75d64b943a4b51837abdb6f9c8643fe028d7
Steve Krueger <steve@thejibe.com>
2011-07-05 11:43:23 -0700

    updating HEAD to 7.x-2.x for branching to 3.x

--- template.php
+++ template.php
@@ -1,215 +1,152 @@
 <?php
 
-//
-//	from ZEN // Override or insert PHPTemplate variables into the page templates.
-//	
-//	 This function creates the body classes that are relative to each page
-//	
-//	@param $vars
-//	  A sequential array of variables to pass to the theme template.
-//	@param $hook
-//	  The name of the theme function being called ("page" in this case.)
-//
-
-function phptemplate_preprocess_page(&$vars, $hook) {
-  global $theme;
-
-  // Don't display empty help from node_help().
-  if ($vars['help'] == "<div class=\"help\"><p></p>\n</div>") {
-    $vars['help'] = '';
-  }
-
-  // Classes for body element. Allows advanced theming based on context
-  // (home page, node of certain type, etc.)
-  $body_classes = array($vars['body_classes']);
-  if (!$vars['is_front']) {
-    // Add unique classes for each page and website section
-    $path = drupal_get_path_alias($_GET['q']);
-    list($section, ) = explode('/', $path, 2);
-    $body_classes[] = phptemplate_id_safe('page-'. $path);
-    $body_classes[] = phptemplate_id_safe('section-'. $section);
-    if (arg(0) == 'node') {
-      if (arg(1) == 'add') {
-        if ($section == 'node') {
-          array_pop($body_classes); // Remove 'section-node'
-        }
-        $body_classes[] = 'section-node-add'; // Add 'section-node-add'
-      }
-      elseif (is_numeric(arg(1)) && (arg(2) == 'edit' || arg(2) == 'delete')) {
-        if ($section == 'node') {
-          array_pop($body_classes); // Remove 'section-node'
-        }
-        $body_classes[] = 'section-node-'. arg(2); // Add 'section-node-edit' or 'section-node-delete'
-      }
-    }
-  }
-  $vars['body_classes'] = implode(' ', $body_classes); // Concatenate with spaces
-}
-
-
-
-//
-//	from ZEN // Override or insert PHPTemplate variables into the node templates.
-//	
-//	 This function creates the NODES classes, like 'node-unpublished' for nodes
-//	 that are not published, or 'node-mine' for node posted by the connected user...
-//	
-//	@param $vars
-//	  A sequential array of variables to pass to the theme template.
-//	@param $hook
-//	  The name of the theme function being called ("node" in this case.)
-//
-
-function phptemplate_preprocess_node(&$vars, $hook) {
-  global $user;
-
-  // Special classes for nodes
-  $node_classes = array();
-  if ($vars['sticky']) {
-    $node_classes[] = 'sticky';
-  }
-  if (!$vars['node']->status) {
-    $node_classes[] = 'node-unpublished';
-    $vars['unpublished'] = TRUE;
-  }
-  else {
-    $vars['unpublished'] = FALSE;
-  }
-  if ($vars['node']->uid && $vars['node']->uid == $user->uid) {
-    // Node is authored by current user
-    $node_classes[] = 'node-mine';
-  }
-  if ($vars['teaser']) {
-    // Node is displayed as teaser
-    $node_classes[] = 'node-teaser';
-  }
-  // Class for node type: "node-type-page", "node-type-story", "node-type-my-custom-type", etc.
-  $node_classes[] = 'node-type-'. $vars['node']->type;
-  $vars['node_classes'] = implode(' ', $node_classes); // Concatenate with spaces
-}
-
-
-//
-// from ZEN // Override or insert PHPTemplate variables into the block templates.
-//
-//	This function create the EDIT LINKS for blocks and menus blocks.
-//	When overing a block (except in IE6), some links appear to edit
-//	or configure the block. You can then edit the block, and once you are
-//	done, brought back to the first page.
-//
-// @param $vars
-//   A sequential array of variables to pass to the theme template.
-// @param $hook
-//   The name of the theme function being called ("block" in this case.)
-// 
-
-function phptemplate_preprocess_block(&$vars, $hook) {
-  $block = $vars['block'];
-
-  if (user_access('administer blocks')) {
-    // Display 'edit block' for custom blocks
-    if ($block->module == 'block') {
-      $edit_links[] = l( t('edit block'), 'admin/build/block/configure/'. $block->module .'/'. $block->delta, array('title' => t('edit the content of this block'), 'class' => 'block-edit'), drupal_get_destination(), NULL, FALSE, TRUE);
-    }
-    // Display 'configure' for other blocks
-    else {
-      $edit_links[] = l(t('configure'), 'admin/build/block/configure/'. $block->module .'/'. $block->delta, array('title' => t('configure this block'), 'class' => 'block-config'), drupal_get_destination(), NULL, FALSE, TRUE);
-    }
-
-    // Display 'edit menu' for menu blocks
-    if (($block->module == 'menu' || ($block->module == 'user' && $block->delta == 1)) && user_access('administer menu')) {
-      $edit_links[] = l(t('edit menu'), 'admin/build/menu', array('title' => t('edit the menu that defines this block'), 'class' => 'block-edit-menu'), drupal_get_destination(), NULL, FALSE, TRUE);
-    }
-    $vars['edit_links_array'] = $edit_links;
-    $vars['edit_links'] = '<div class="edit">'. implode(' ', $edit_links) .'</div>';
-  }
-}
-
-
-//
-//  Create some custom classes for comments
-//
-
-function comment_classes($comment) {
-  $node = node_load($comment->nid);
-  global $user;
+/**
+ * Here we override the default HTML output of drupal.
+ * refer to http://drupal.org/node/550722
+ */
  
-  $output .= ($comment->new) ? ' comment-new' : ''; 
-  $output .=  ' '. $status .' '; 
-  if ($node->name == $comment->name) {	
-    $output .= 'node-author';
-  }
-  if ($user->name == $comment->name) {	
-    $output .=  ' mine';
-  }
-  return $output;
+// Auto-rebuild the theme registry during theme development.
+if (theme_get_setting('clear_registry')) {
+  // Rebuild .info data.
+  system_rebuild_theme_data();
+  // Rebuild theme registry.
+  drupal_theme_rebuild();
+}
+// Add Zen Tabs styles
+if (theme_get_setting('basic_tabs')) {
+  drupal_add_css( drupal_get_path('theme', 'basic') .'/css/tabs.css');
 }
 
-
-// 	
-// 	Customize the PRIMARY and SECONDARY LINKS, to allow the admin tabs to work on all browsers
-// 	Generate the HTML representing a given menu item ID.
-// 	An implementation of theme_menu_item_link()
-// 	
-// 	@param $link
-// 	  array The menu item to render.
-// 	@return
-// 	  string The rendered menu item.
-// 	
-
-function phptemplate_menu_item_link($link) {
-  if (empty($link['options'])) {
-    $link['options'] = array();
+function basic_preprocess_page(&$vars, $hook) {
+  if (isset($vars['node_title'])) {
+    $vars['title'] = $vars['node_title'];
+  }
+  // Adding a class to #page in wireframe mode
+  if (theme_get_setting('wireframe_mode')) {
+    $vars['classes_array'][] = 'wireframe-mode';
+  }
+  // Adding classes wether #navigation is here or not
+  if (!empty($vars['main_menu']) or !empty($vars['sub_menu'])) {
+    $vars['classes_array'][] = 'with-navigation';
+  }
+  if (!empty($vars['secondary_menu'])) {
+    $vars['classes_array'][] = 'with-subnav';
   }
 
-  // If an item is a LOCAL TASK, render it as a tab
-  if ($link['type'] & MENU_IS_LOCAL_TASK) {
-    $link['title'] = '<span class="tab">'. check_plain($link['title']) .'</span>';
-    $link['options']['html'] = TRUE;
+  // Add first/last classes to node listings about to be rendered.
+  if (isset($vars['page']['content']['system_main']['nodes'])) {
+    // All nids about to be loaded (without the #sorted attribute).
+    $nids = element_children($vars['page']['content']['system_main']['nodes']);
+    // Only add first/last classes if there is more than 1 node being rendered.
+    if (count($nids) > 1) {
+      $first_nid = reset($nids);
+      $last_nid = end($nids);
+      $first_node = $vars['page']['content']['system_main']['nodes'][$first_nid]['#node'];
+      $first_node->classes_array = array('first');
+      $last_node = $vars['page']['content']['system_main']['nodes'][$last_nid]['#node'];
+      $last_node->classes_array = array('last');
+    }
   }
+}
 
-  if (empty($link['type'])) {
-    $true = TRUE;
+function basic_preprocess_node(&$vars) {
+  // Add a striping class.
+  $vars['classes_array'][] = 'node-' . $vars['zebra'];
+
+  // Merge first/last class (from basic_preprocess_page) into classes array of current node object.
+  $node = $vars['node'];
+  if (!empty($node->classes_array)) {
+    $vars['classes_array'] = array_merge($vars['classes_array'], $node->classes_array);
   }
+}
 
-  return l($link['title'], $link['href'], $link['options']);
+function basic_preprocess_block(&$vars, $hook) {
+  // Add a striping class.
+  $vars['classes_array'][] = 'block-' . $vars['block_zebra'];
+
+  // Add first/last block classes
+  $first_last = "";
+  // If block id (count) is 1, it's first in region.
+  if ($vars['block_id'] == '1') {
+    $first_last = "first";
+    $vars['classes_array'][] = $first_last;
+  }
+  // Count amount of blocks about to be rendered in that region.
+  $block_count = count(block_list($vars['elements']['#block']->region));
+  if ($vars['block_id'] == $block_count) {
+    $first_last = "last";
+    $vars['classes_array'][] = $first_last;
+  }
 }
 
 /**
- * Duplicate of theme_menu_local_tasks() but adds clear-block to tabs.
+ * Return a themed breadcrumb trail.
+ *
+ * @param $breadcrumb
+ *   An array containing the breadcrumb links.
+ * @return
+ *   A string containing the breadcrumb output.
  */
-function phptemplate_menu_local_tasks() {
-  $output = '';
+function basic_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  // Determine if we are to display the breadcrumb.
+  $show_breadcrumb = theme_get_setting('basic_breadcrumb');
+  if ($show_breadcrumb == 'yes' || $show_breadcrumb == 'admin' && arg(0) == 'admin') {
 
-  if ($primary = menu_primary_local_tasks()) {
-    $output .= "<ul class=\"tabs primary clear-block\">\n". $primary ."</ul>\n";
-  }
-  if ($secondary = menu_secondary_local_tasks()) {
-    $output .= "<ul class=\"tabs secondary clear-block\">\n". $secondary ."</ul>\n";
-  }
+    // Optionally get rid of the homepage link.
+    $show_breadcrumb_home = theme_get_setting('basic_breadcrumb_home');
+    if (!$show_breadcrumb_home) {
+      array_shift($breadcrumb);
+    }
 
-  return $output;
+    // Return the breadcrumb with separators.
+    if (!empty($breadcrumb)) {
+      $breadcrumb_separator = theme_get_setting('basic_breadcrumb_separator');
+      $trailing_separator = $title = '';
+      if (theme_get_setting('basic_breadcrumb_title')) {
+        $item = menu_get_item();
+        if (!empty($item['tab_parent'])) {
+          // If we are on a non-default tab, use the tab's title.
+          $title = check_plain($item['title']);
+        }
+        else {
+          $title = drupal_get_title();
+        }
+        if ($title) {
+          $trailing_separator = $breadcrumb_separator;
+        }
+      }
+      elseif (theme_get_setting('basic_breadcrumb_trailing')) {
+        $trailing_separator = $breadcrumb_separator;
+      }
+
+      // Provide a navigational heading to give context for breadcrumb links to
+      // screen-reader users. Make the heading invisible with .element-invisible.
+      $heading = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+      return $heading . '<div class="breadcrumb">' . implode($breadcrumb_separator, $breadcrumb) . $trailing_separator . $title . '</div>';
+    }
+  }
+  // Otherwise, return an empty string.
+  return '';
 }
 
-
-//	
-//	Converts a string to a suitable html ID attribute.
-//	
-//	 http://www.w3.org/TR/html4/struct/global.html#h-7.5.2 specifies what makes a
-//	 valid ID attribute in HTML. This function:
-//	
-//	- Ensure an ID starts with an alpha character by optionally adding an 'n'.
-//	- Replaces any character except A-Z, numbers, and underscores with dashes.
-//	- Converts entire string to lowercase.
-//	
-//	@param $string
-//	  The string
-//	@return
-//	  The converted string
-//	
-
-
-function phptemplate_id_safe($string) {
+/**
+ * Converts a string to a suitable html ID attribute.
+ *
+ * http://www.w3.org/TR/html4/struct/global.html#h-7.5.2 specifies what makes a
+ * valid ID attribute in HTML. This function:
+ *
+ * - Ensure an ID starts with an alpha character by optionally adding an 'n'.
+ * - Replaces any character except A-Z, numbers, and underscores with dashes.
+ * - Converts entire string to lowercase.
+ *
+ * @param $string
+ * 	The string
+ * @return
+ * 	The converted string
+ */	
+function basic_id_safe($string) {
   // Replace with dashes anything that isn't A-Z, numbers, dashes, or underscores.
   $string = strtolower(preg_replace('/[^a-zA-Z0-9_-]+/', '-', $string));
   // If the first character is not a-z, add 'n' in front.
@@ -219,34 +156,67 @@ function phptemplate_id_safe($string) {
   return $string;
 }
 
-// 
-// from STEVE KRUEGER truncate text characters
-//
-//	You can use this function in the node templates to maximize the number of words
-//	of an item. For example, if you wish to have a teaser of the body, limited to
-//	15 words, use this : 
-//
-//	print truncate($node->body,15)
-//
-//	This function also remove all markup, like <a> or <strong> to preserve the integrity
-//	of the markup
+/**
+ * Generate the HTML output for a menu link and submenu.
+ *
+ * @param $variables
+ *  An associative array containing:
+ *   - element: Structured array data for a menu link.
+ *
+ * @return
+ *  A themed HTML string.
+ *
+ * @ingroup themeable
+ * 
+ */
+function basic_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
 
-
-function truncate($phrase, $max_words) {
-	$phrase = strip_tags($phrase);
-  $phrase_array = explode(' ', $phrase);
-  if(count($phrase_array) > $max_words && $max_words > 0)
-    $phrase = implode(' ', array_slice($phrase_array, 0, $max_words)) .'...'; 
-  return $phrase;
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  // Adding a class depending on the TITLE of the link (not constant)
+  $element['#attributes']['class'][] = basic_id_safe($element['#title']);
+  // Adding a class depending on the ID of the link (constant)
+  $element['#attributes']['class'][] = 'mid-' . $element['#original_link']['mlid'];
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
-//
-//  Return a themed breadcrumb trail.
-//	Alow you to customize the breadcrumb markup
-//
+/**
+ * Override or insert variables into theme_menu_local_task().
+ */
+function basic_preprocess_menu_local_task(&$variables) {
+  $link =& $variables['element']['#link'];
 
-function phptemplate_breadcrumb($breadcrumb) {
-  if (!empty($breadcrumb)) {
-    return '<div class="breadcrumb">'. implode(' » ', $breadcrumb) .'</div>';
+  // If the link does not contain HTML already, check_plain() it now.
+  // After we set 'html'=TRUE the link will not be sanitized by l().
+  if (empty($link['localized_options']['html'])) {
+    $link['title'] = check_plain($link['title']);
   }
+  $link['localized_options']['html'] = TRUE;
+  $link['title'] = '<span class="tab">' . $link['title'] . '</span>';
+}
+
+/**
+ * Duplicate of theme_menu_local_tasks() but adds clearfix to tabs.
+ */
+function basic_menu_local_tasks(&$variables) {  
+  $output = '';
+
+  if (!empty($variables['primary'])) {
+    $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+    $variables['primary']['#prefix'] .= '<ul class="tabs primary clearfix">';
+    $variables['primary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables['primary']);
+  }
+  if (!empty($variables['secondary'])) {
+    $variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+    $variables['secondary']['#prefix'] .= '<ul class="tabs secondary clearfix">';
+    $variables['secondary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables['secondary']);
+  }
+
+  return $output;
 }
