9a9e89d77439c52c87258ee7fbddb176f82dd286
Steve Krueger <steve@thejibe.com>
2012-06-20 09:39:32 -0700

    fixing #1356236

--- template.php
+++ template.php
@@ -148,11 +148,7 @@ function basic_breadcrumb($variables) {
  */	
 function basic_id_safe($string) {
   // Replace with dashes anything that isn't A-Z, numbers, dashes, or underscores.
-  $string = strtolower(preg_replace('/[^a-zA-Z0-9_-]+/', '-', $string));
-  // If the first character is not a-z, add 'n' in front.
-  if (!ctype_lower($string{0})) { // Don't use ctype_alpha since its locale aware.
-    $string = 'id'. $string;
-  }
+  $string = drupal_html_id($string);
   return $string;
 }
 
