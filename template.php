d1418b3df70ca9dc1291bda710d49afd58313f0b
Joel Pittet <joel@pittet.ca>
2013-02-23 16:32:35 -0800

    Issue #1778676 by dubcanada: fixes missing  variable

--- template.php
+++ template.php
@@ -69,6 +69,9 @@ function basic_preprocess_node(&$vars) {
   // Add a striping class.
   $vars['classes_array'][] = 'node-' . $vars['zebra'];
 
+  // Add $unpublished variable.
+  $vars['unpublished'] = (!$vars['status']) ? TRUE : FALSE;
+
   // Merge first/last class (from basic_preprocess_page) into classes array of current node object.
   $node = $vars['node'];
   if (!empty($node->classes_array)) {
