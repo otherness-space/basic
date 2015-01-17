cce4f8b6dfdacea54f883772fc413b13aee0d7eb
Steve Krueger <steve@thejibe.com>
2015-01-17 15:39:04 -0800

    various small bug fixes

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -5,31 +5,27 @@
   <header id="header">
     <div class="container">
       <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+        <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>"/>
         </a>
       <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-              </div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-              </h1>
-            <?php endif; ?>
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+            <div id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home"><?php print $site_name; ?></a>
+            </div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home"><?php print $site_name; ?></a>
+            </h1>
           <?php endif; ?>
+        <?php endif; ?>
 
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-
-        </div>
+        <?php if ($site_slogan): ?>
+          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
       <?php endif; ?>
 
       <?php if ($page['header']): ?>
@@ -44,8 +40,8 @@
     <nav id="navigation" class="menu <?php if (!empty($main_menu)) {print "with-primary";}
       if (!empty($secondary_menu)) {print " with-secondary";} ?>">
       <div class="container">
-        <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu')))); ?>
-        <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary', 'class' => array('links', 'clearfix', 'sub-menu')))); ?>
+        <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('main-menu')))); ?>
+        <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary', 'class' => array('sub-menu')))); ?>
       </div>
     </nav> <!-- /navigation -->
   <?php endif; ?>
@@ -75,7 +71,7 @@
               <?php print $messages; ?>
               <?php print render($page['help']); ?>
 
-              <?php if ($tabs): ?>
+              <?php if (render($tabs)): ?>
                 <div class="tabs"><?php print render($tabs); ?></div>
               <?php endif; ?>
 
